6d9362012cbcaf63c59383d92506cbc3c3992fa5
grixawol <garimangangwani@gmail.com>
2023-04-12 09:06:51 +0530

    ip

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import tkinter.font as tkFont
 import time
 import threading
 I_C=-1
-SERVER_IP   = '192.168.35.197'
+SERVER_IP   = '192.168.11.197'
 PORT_NUMBER = 5000
 SIZE = 1024
 print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
