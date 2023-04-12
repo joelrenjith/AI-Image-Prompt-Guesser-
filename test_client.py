707c691205c043a6723dfa95431021197d408ba8
Joel Renjith <joelrenjith9@gmail.com>
2023-04-12 12:37:06 +0530

    test_multi_6

--- test_client.py
+++ test_client.py
@@ -1,23 +1,20 @@
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM
-SERVER_IP   = '192.168.11.206'
+SERVER_IP   = '127.0.0.1'
 PORT_NUMBER = 5000
 SIZE = 1024
 print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
 try:
     mySocket = socket( AF_INET, SOCK_DGRAM )
-    myMessage = "Hello!"
+    username = input('enter username')
+    # myMessage = "Hello!"
     myMessage1 = ""
     i = 0
-    while i < 10:
-        mySocket.sendto(myMessage.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
-        i = i + 1
+    # while i < 10:
+    #     mySocket.sendto(myMessage.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
+    #     i = i + 1
 
-    mySocket.sendto(myMessage1.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
-    print(mySocket.recv(1024).decode())
-    user = 'joel'
-    mySocket.sendto(user.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
-    print('sent username')
+    mySocket.sendto(username.encode('utf-8'),(SERVER_IP,PORT_NUMBER))
     ans  = mySocket.recv(1024)
     print(ans.decode())
 except Exception as e:
--- test_server.py
+++ test_server.py
@@ -1,16 +1,16 @@
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 import sys, threading
-PORT_NUMBER = 5000
+PORT_NUMBER = 5656
 SIZE = 1024
 connected = []
 try:
-    def multi_threaded_client(connection):
+    def multi_threaded_client(connection,username):
         print(connection)
-        mySocket.sendto(('enter username').encode(),(connection))
-        username,connection  = mySocket.recvfrom(1024)
-        username = username.decode()
-        print('recieved username')
         mySocket.sendto(('okay your user name is ='+username).encode(),(connection))
+        
+
+        print('recieved username',username)
+        
     ThreadCount = 0
     hostName = gethostbyname( '0.0.0.0' )
     mySocket = socket( AF_INET, SOCK_DGRAM )
@@ -25,9 +25,10 @@ try:
         Client, address = mySocket.recvfrom(1024)
         if address not in connected:
             connected.append(address)
+            print(Client.decode())
             print('Connected to: ' + address[0] + ':' + str(address[1]))
             
-            threading.Thread(target = multi_threaded_client,args= (address,)).start()
+            threading.Thread(target = multi_threaded_client,args= (address,Client.decode(),)).start()
             
             ThreadCount += 1
             print('Thread Number: ' + str(ThreadCount))
