30155bda78f6305b999bd2763a0fec9a7cafa31d
Joel Renjith <joelrenjith9@gmail.com>
2023-04-18 13:50:56 +0530

    selenium_thraed

---
+++ test_selenium_thread.py
@@ -0,0 +1,55 @@
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+# dic = {'frixionmaster@gmail.com':'hello12345678','joelrenjith10@gmail.com':'JPYVDTLX','garimangangwani@gmail.com':'CJNYJAMN','diyx19@gmail.com':'JFLBAUEC'}
+# ch = random.choice(list(dic))
+# print(ch)
+# df = pd.read_csv(r'Skribbl-words.csv')
+# df_new = (df['word'])
+# PATH = Service('C:\Program Files (x86)\chromedriver.exe')
+# options = Options()
+# options.binary_location = r'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe'
+# options.accept_insecure_certs = True
+# options.headless = True
+# driver = webdriver.Chrome( service = PATH,options = options)
+# driver.get('https://freeimagegenerator.com/')
+# print('opened website --> waiting for sign in')
+# sign = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/nav/div[3]/div/div[2]/ul/li/a"))).click()
+# art = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/div/a[1]"))).click()
+# user = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/form/div/div[1]/input"))).send_keys(ch)
+# pswrd = driver.find_element(By.XPATH,"/html/body/div[2]/div/form/div/div[2]/input")
+# pswrd.send_keys(dic[ch])
+# login  =driver.find_element(By.XPATH,"/html/body/div[2]/div/form/div/div[3]/a")
+# login.click()
+# print('Sign-in done')
+# head = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,"/html/body/header/div/div/div[1]/a[1]"))).click()
+# prompt=  WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[2]/div[2]/div[1]/div[1]/input")))
+# l = list(df_new.sample(n=2))
+# s=" ".join(l)
+# s= s.lower()
+# print(s)
+# copy = ''
+# prompt.send_keys(s)
+# prompt.send_keys(Keys.ENTER)
+# print('Enterred prompt')
+# img = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[2]/div[6]/button[1]"))).click()
+# print('got results --> waiting for image')
+# element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/div[2]/div[6]/a/img")))
+# src  = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div[6]/a/img").get_attribute("src")
+
+# print(src+ "\n\n")
+import pandas as pd
+df = pd.read_csv(r'words&imgs.csv')
+# df1 = pd.DataFrame({'string':['apple'],'img_link': ['gay']})
+# df1.to_csv(r'words&imgs.csv',mode='a', index=False, header=False)
+print(df)
+df = df.drop(0)
+print("\n\n",df)
+df.to_csv(r'words&imgs.csv',index=False)
+# print
