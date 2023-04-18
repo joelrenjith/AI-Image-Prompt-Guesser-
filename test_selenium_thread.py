ca1386510d40a9ddd63b626d29cbee291d22a1c1
Diya Maria Abraham <diya.maria19@gmail.com>
2023-04-18 16:50:38 +0530

    sel_test

--- test_selenium_thread.py
+++ test_selenium_thread.py
@@ -44,12 +44,68 @@ import pandas as pd
 # src  = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div[6]/a/img").get_attribute("src")
 
 # print(src+ "\n\n")
-import pandas as pd
-df = pd.read_csv(r'words&imgs.csv')
-# df1 = pd.DataFrame({'string':['apple'],'img_link': ['gay']})
-# df1.to_csv(r'words&imgs.csv',mode='a', index=False, header=False)
-print(df)
-df = df.drop(0)
-print("\n\n",df)
-df.to_csv(r'words&imgs.csv',index=False)
-# print
+# import pandas as pd
+# df = pd.read_csv(r'words&imgs.csv')
+# # df1 = pd.DataFrame({'string':['apple'],'img_link': ['gay']})
+# # df1.to_csv(r'words&imgs.csv',mode='a', index=False, header=False)
+# print(df)
+# df = df.drop(0)
+# print("\n\n",df)
+# df.to_csv(r'words&imgs.csv',index=False)
+# # print
+
+try:
+
+    dic = {'frixionmaster@gmail.com':'hello12345678','joelrenjith10@gmail.com':'JPYVDTLX','garimangangwani@gmail.com':'CJNYJAMN','diyx19@gmail.com':'JFLBAUEC'}
+    ch = random.choice(list(dic))
+
+    df = pd.read_csv('cn_project_1\Skribbl-words.csv')
+    df_new = (df['word'])
+
+    #df = pd.read_csv(r'words&imgs.csv')
+
+    PATH = Service('C:\Program Files (x86)\chromedriver.exe')
+    options = Options()
+    #options.binary_location = r'C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe'
+    options.accept_insecure_certs = True
+    #options.headless = True
+    driver = webdriver.Chrome( service = PATH,options = options)
+    driver.get('https://freeimagegenerator.com/')
+    sign = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/nav/div[3]/div/div[2]/ul/li/a"))).click()
+    art = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/div/a[1]"))).click()
+    user = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/form/div/div[1]/input"))).send_keys(ch)
+    pswrd = driver.find_element(By.XPATH,"/html/body/div[2]/div/form/div/div[2]/input")
+    pswrd.send_keys(dic[ch])
+    login  =driver.find_element(By.XPATH,"/html/body/div[2]/div/form/div/div[3]/a")
+    login.click()
+    head = WebDriverWait(driver,10).until(EC.visibility_of_element_located((By.XPATH,"/html/body/header/div/div/div[1]/a[1]"))).click()
+
+    i = 1
+    temp =[]
+
+    def generateString(x):
+        l = list(x.sample(n=2))
+        s=" ".join(l)
+        return s
+
+    while(i<5):
+        s = generateString(df_new)
+        if i>0:
+            retry = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/div[1]/div/a")))
+            retry.click()
+        prompt=  WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[2]/div[2]/div[1]/div[1]/input")))
+        prompt.send_keys(s)
+        prompt.send_keys(Keys.ENTER)
+        img = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[3]/div/div[2]/div[6]/button[1]"))).click()
+        element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/div[2]/div[4]/a/img")))
+        src  = driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div[4]/a/img").get_attribute("src")
+        #print(src+ "\n\n")
+        entry = {'string':s,'link':src}
+        temp.append(entry)
+        df = pd.DataFrame(temp)
+        df.to_csv('cn_project_1\words&imgs.csv',index=False)
+        print(f'added image {i}')
+        i+=1
+
+except Exception as e:
+    print(e)
