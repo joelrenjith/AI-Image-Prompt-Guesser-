cc2e56bbf31dd230d5295966538ddb1dc90ad523
grixawol <garimangangwani@gmail.com>
2023-04-19 10:34:10 +0530

    multiclient changes in client

--- test_multiclient.py
+++ test_multiclient.py
@@ -3,20 +3,21 @@ from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 import threading
 PORT_NUMBER = 5000
 SIZE = 1024
-hostName = '127.0.0.1'
+hostName = '192.168.11.197'
+# hostName = '127.0.0.1'
 # mySocket = socket( AF_INET, SOCK_DGRAM )
 mySocket = socket( AF_INET, SOCK_DGRAM )
 adr = (hostName,PORT_NUMBER)
 user = input('enter username')
 mySocket.sendto(user.encode(),adr)
 
-def ready():
-    one  = input('enter 1 to get ready')
-    mySocket.sendto(one.encode(),adr)
-    return
+# def ready():
+#     one  = input('enter 1 to get ready')
+#     mySocket.sendto(one.encode(),adr)
+#     return
 
-t1 = threading.Thread(target=ready())
-t1.start()
+# t1 = threading.Thread(target=ready())
+# t1.start()
 
 print('list of players:\n__________________________________')
 while(1):
